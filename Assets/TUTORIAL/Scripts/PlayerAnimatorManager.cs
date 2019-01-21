14dcdd04bac61db0dc4a0cc2755b1c7ac487c45a
Unknown <bendzz@live.co.uk>
2019-01-21 15:09:05 -0700

    Following Tutorial
    right about here atm https://doc.photonengine.com/en-us/pun/v1/demos-and-tutorials/pun-basics-tutorial/player-networking

---
+++ Assets/TUTORIAL/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Mike.QuickMP
+{
+    public class PlayerAnimatorManager : MonoBehaviour
+    {
+        #region PUBLIC PROPERTIES
+        public float DirectionDampTime = .25f;
+        #endregion
+
+        #region PRIVATE PROPERTIES
+        private Animator animator;
+        #endregion
+
+        #region MONOBEHAVIOUR MESSAGES
+
+        // Use this for initialization
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+            if (!animator)
+            {
+                Debug.LogError("PlayerAnimatorManager is Missing Animator Component", this);
+            }
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!animator)
+            {
+                return;
+            }
+
+            // deal with Jumping
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            // only allow jumping if we are running.
+            if (stateInfo.IsName("Base Layer.Run"))
+            {
+                // When using trigger parameter
+                if (Input.GetButtonDown("Fire2"))
+                {
+                    animator.SetTrigger("Jump");
+                }
+            }
+
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+
+            if (v < 0)
+            {
+                v = 0;
+            }
+
+            animator.SetFloat("Speed", h * h + v * v);
+
+            animator.SetFloat("Direction", h, DirectionDampTime, Time.deltaTime);
+        }
+
+
+        #endregion
+    }
+}
---
+++ Assets/TUTORIAL/Scripts/PlayerNameInputField.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+namespace Mike.QuickMP
+{
+    /// <summary>
+    /// Player name input field. Let the user input his name, will appear above the player in the game.
+    /// </summary>
+    [RequireComponent(typeof(InputField))]
+    public class PlayerNameInputField : MonoBehaviour
+    {
+        #region Private Variables
+
+        // Store the PlayerPref Key to avoid typos
+        static string playerNamePrefKey = "PlayerName";
+
+        #endregion
+
+
+        #region MonoBehaviour CallBacks
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during initialization phase.
+        /// </summary>
+        void Start()
+        {
+
+            string defaultName = "";
+            InputField _inputField = this.GetComponent<InputField>();
+            if (_inputField != null)
+            {
+                if (PlayerPrefs.HasKey(playerNamePrefKey))
+                {
+                    defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                    _inputField.text = defaultName;
+                }
+            }
+
+
+            PhotonNetwork.playerName = defaultName;
+        }
+
+
+        #endregion
+
+
+        #region Public Methods
+
+
+        /// <summary>
+        /// Sets the name of the player and save it in the PlayerPrefs for future sessions.
+        /// </summary>
+        /// <param name="value">The name of the Player</param>
+        //public void SetPlayerName(string value)
+        public void SetPlayerName(InputField value)
+        {
+            // #Important
+            PhotonNetwork.playerName = value + " "; // force a trailing space string in case value is an empty string, else playerName would not be updated.
+
+
+            //PlayerPrefs.SetString(playerNamePrefKey, value);
+            PlayerPrefs.SetString(playerNamePrefKey, value.text);
+
+            // had to rewrite this function using this https://www.reddit.com/r/Unity3D/comments/66jc06/how_to_get_inputfield_value/dgjc1nc/  didn't get any text using the tutorial's method
+            //Debug.Log("name set to " + value.text);
+        }
+
+
+        #endregion
+    }
+}
