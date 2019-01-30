5a838643ff6b9061291a3fb8b42f52273f6687fd
Unknown <bendzz@live.co.uk>
2019-01-29 19:53:44 -0700

    basic PUN position and velocity streaming working
    I had to rewrite the default transform sync system cause the easy options were shitty (lerping etc) and I couldn't get good velocity values to send since I didn't know the update rate/timing.
    Still need to add rotational syncing and split the position error correction velocity to be a separate variable

--- Assets/JUMPER/JumperScript.cs
+++ Assets/JUMPER/JumperScript.cs
@@ -35,10 +35,22 @@ public class JumperScript : Photon.PunBehaviour, IPunObservable
     private Quaternion jumperPlateStartRotation;
     private bool _isGrounded = false;
 
+    /*
     private Vector3 lastPosition, velocity;
     private Quaternion lastRotation;
     private float turnSpeed;
     float lastTime = 0;
+    */
+    private Vector3 targetPosition;
+    private Quaternion targetRotation;
+    private Vector3 oldTargetPosition;
+    private Quaternion oldTargetRotation;
+    private Vector3 velocity;
+    private Vector3 netSyncVelocity;
+    private float netTickDelta;
+    private float netTickLast;
+    private float oldSenderTime;
+    private float senderTime;
 
     Camera _jumperCamera;
     #endregion
@@ -62,9 +74,15 @@ public class JumperScript : Photon.PunBehaviour, IPunObservable
             _jumperCamera.enabled = false;
         }
 
-        lastPosition = transform.position;
-        lastRotation = transform.rotation;
-    }
+        //lastPosition = transform.position;
+        //lastRotation = transform.rotation;
+
+        oldTargetPosition = targetPosition = transform.position;
+        oldTargetRotation = targetRotation = transform.rotation;
+        velocity = new Vector3(0, 0, 0);
+        netSyncVelocity = new Vector3(0, 0, 0);
+        netTickDelta = 0.1f;
+}
 
     // Update is called once per frame
     void Update()
@@ -99,46 +117,63 @@ public class JumperScript : Photon.PunBehaviour, IPunObservable
 
         /////
         // Below stuff only happens on THIS client's computer, not other players!
-        if (photonView.isMine == false && PhotonNetwork.connected == true) { return; }
-
-        if (Cursor.lockState != CursorLockMode.Locked) { Cursor.lockState = CursorLockMode.Locked; }
-        if (Cursor.visible) { Cursor.visible = false; }
-
-        _head.transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0, 0));
-
-
-        desiredMove = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) / 3;
-        desiredMove = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0) * (desiredMove * maxSpeed);
-
-        move = new Vector3();
-
-
-        if ((desiredMove.x + desiredMove.z) % 1 != 0)
+        //if (photonView.isMine == false && PhotonNetwork.connected == true) { return; }
+        if (photonView.isMine == true || PhotonNetwork.connected == false)
         {
-            // input is from joystick, don't smooth it
-            move = desiredMove;
-        } else
+
+            if (Cursor.lockState != CursorLockMode.Locked) { Cursor.lockState = CursorLockMode.Locked; }
+            if (Cursor.visible) { Cursor.visible = false; }
+
+            _head.transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0, 0));
+
+
+            desiredMove = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) / 3;
+            desiredMove = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0) * (desiredMove * maxSpeed);
+
+            move = new Vector3();
+
+
+            if ((desiredMove.x + desiredMove.z) % 1 != 0)
+            {
+                // input is from joystick, don't smooth it
+                move = desiredMove;
+            }
+            else
+            {
+                // input is from keyboard (or a joystick at 100%), DO smooth it
+                move = desiredMove;
+                //Debug.Log(move);
+            }
+
+            //_groundDetecter.
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                //_body.AddForce(Vector3.up * jumpStrength, ForceMode.VelocityChange);
+                //_body.velocity.Set(0, 10, 0);
+                _body.velocity = new Vector3(0, jumpStrength, 0);
+            }
+
+            transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0));
+            //_head.transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0, 0));
+
+            //PhotonNetwork.ServerTimestamp
+            //Debug.Log(Time.fixedTime);
+        } else if (photonView.isMine == false && PhotonNetwork.connected == true)
         {
-            // input is from keyboard (or a joystick at 100%), DO smooth it
-            move = desiredMove;
-            //Debug.Log(move);
+            //if ((Time.time - netTickLast) < netTickTime)
+            //{
+            //velocity += netSyncVelocity * netTickTime;
+            //transform.position += velocity;
+            //transform.position = oldTargetPosition + netSyncVelocity * ((Time.time - netTickLast) / netTickTime);
+            //transform.position = oldTargetPosition + netSyncVelocity * Time.deltaTime;
+            transform.position += netSyncVelocity * Time.deltaTime;
+            //} else
+            //{
+            //    transform.position = targetPosition;
+            //    transform.rotation = targetRotation;
+            //}
         }
-
-        //_groundDetecter.
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            //_body.AddForce(Vector3.up * jumpStrength, ForceMode.VelocityChange);
-            //_body.velocity.Set(0, 10, 0);
-            _body.velocity = new Vector3(0, jumpStrength, 0);
-        }
-
-        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0));
-        //_head.transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0, 0));
-
-        //PhotonNetwork.ServerTimestamp
-        //Debug.Log(Time.fixedTime);
-
     }
 
     private void FixedUpdate()
@@ -153,11 +188,11 @@ public class JumperScript : Photon.PunBehaviour, IPunObservable
             _body.AddForce(-Vector3.up * gravityAccel, ForceMode.Acceleration);
         }
 
-
+        /*
         if (photonView.isMine == true && PhotonNetwork.connected == true)
         {
             // Note: This is inconsistently jerky, but seems to handle fast changes better
-            // I think it can work if I can just get the god damn network update ticks to time it better...!
+            // I think it can work if I can just get the god damn network update ticks, to time it better...!
             velocity = (transform.position - lastPosition) / Time.fixedDeltaTime; //Time.deltaTime;
             turnSpeed = Quaternion.Angle(transform.rotation, lastRotation) / Time.fixedDeltaTime; //Time.deltaTime;
             lastPosition = transform.position;
@@ -168,7 +203,7 @@ public class JumperScript : Photon.PunBehaviour, IPunObservable
             gameObject.GetComponent<PhotonTransformView>().SetSynchronizedValues(velocity, turnSpeed);
             //Debug.Log("lastPosition " + velocity + " lastRotation " + turnSpeed);
         }
-
+        */
     }
 
     //on
@@ -187,18 +222,45 @@ private void OnCollisionEnter(Collision collision)
 
     void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        
         if (stream.isWriting)
         {
+            netTickDelta = Time.time - netTickLast;
+            netTickLast = Time.time;
+
+            //velocity = (transform.position - oldTargetPosition) / (Time.time - netTickLast);
+            //oldTargetPosition = transform.position;
+            //netTickLast = Time.time;
+
+            velocity = (transform.position - oldTargetPosition) / netTickDelta;
+            oldTargetPosition = transform.position;
+
             // We own this player: send the others our data
             //stream.SendNext(IsFiring);
             //stream.SendNext(Health);
             //stream.SendNext(_head.transform.rotation.eulerAngles);
             stream.SendNext(_head.transform.rotation);
             //stream.SendNext(Time.fixedTime);
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+            stream.SendNext(velocity);
+            stream.SendNext(Time.time);
+            //Debug.Log("Wrote net stream! Velocity " + velocity);
         }
         else
         {
+            netTickDelta = Time.time - netTickLast;
+            netTickLast = Time.time;
+            oldSenderTime = senderTime;
+
+            //transform.position = targetPosition;    // update to old value first
+            //oldTargetPosition = targetPosition;
+            //transform.rotation = targetRotation;
+            //oldTargetRotation = targetRotation;
+            //oldTargetPosition = transform.position;
+
+            Vector3 positionError = targetPosition - transform.position;
+
+
             // Network player, receive data
             //this.IsFiring = (bool)stream.ReceiveNext();
             //this.Health = (float)stream.ReceiveNext();
@@ -208,11 +270,30 @@ private void OnCollisionEnter(Collision collision)
             //float lastTimeNew = (float)stream.ReceiveNext();
             //if (lastTimeNew != lastTime)
             //{
-                // network data received
-                //lastTimeNew = lastTime;
+            // network data received
+            //lastTimeNew = lastTime;
 
             //}
+            //transform.position = (Vector3)stream.ReceiveNext();
+            //transform.rotation = (Quaternion)stream.ReceiveNext();
+            targetPosition = (Vector3)stream.ReceiveNext();
+            targetRotation = (Quaternion)stream.ReceiveNext();
+            netSyncVelocity = (Vector3)stream.ReceiveNext();
+            senderTime = (float)stream.ReceiveNext();
+            //Debug.Log("Tick! " + targetPosition);
 
+            netSyncVelocity += positionError / 0.1f;
+
+            //float netUpdateDelay
+
+            //netTickTime = 0.1f;
+            //netSyncVelocity = (targetPosition - (transform.position + (velocity * netTickTime)));
+            //netSyncVelocity = (targetPosition - (transform.position )) / netTickTime;
+            //velocity = new Vector3(0, 0, 0);
+
+            Debug.Log("Tick! targetPosition " + targetPosition + " current postion is " + transform.position + " netSyncVelocityDelta " + netSyncVelocity);
+            //Debug.Log("update time " + (senderTime - oldSenderTime));
+            Debug.Log("senderTime - oldSenderTime " + (senderTime - oldSenderTime) + " netTickDelta " + netTickDelta);
         }
 
         //Debug.Log("Network data received.");
